4cd7b5c2a1f66cfc34c79e97eefb6af158c04238
Admin <Admin@Admin-PC>
2020-07-01 20:15:29 +0530

    commit

--- MVC_Assignment/src/com/dxc/dao/StudentDao.java
+++ MVC_Assignment/src/com/dxc/dao/StudentDao.java
@@ -1,5 +1,6 @@
 package com.dxc.dao;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,58 +8,94 @@ import com.dxc.pojos.Student;
 
 public class StudentDao {
 	
-	private List<Student> list=new ArrayList<>();
+	private static Connection conn;
+	
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("driver loaded...");
+			
+		 conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/tushar?autoReconnect=true&useSSL=false", "root", "password");
+			System.out.println("connected to database....");
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 	
 	public void addStudent(Student s){
-		list.add(s);
+		try {
+			PreparedStatement pstmt=conn.prepareStatement("insert into student values(?,?,?)");
+			pstmt.setInt(1, s.getRollno());
+			pstmt.setString(2, s.getName());
+			pstmt.setDouble(3, s.getPercentage());
+			pstmt.executeUpdate();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public List<Student> getAllStudents() {
+		List<Student> list=new ArrayList<>();
+		try {
+			Statement stmt=conn.createStatement();
+			ResultSet rs=stmt.executeQuery("select * from student");
+			while(rs.next())
+			{
+				list.add(new Student(rs.getInt(1),rs.getString(2),rs.getDouble(3)));
+			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 		return list;
 	}
 	
 	public boolean findStudent(int rollno)
 	{
-		for(Student s:list)
-		{
-			if(s.getRollno()==rollno)
+		try {
+			Statement stmt=conn.createStatement();
+			ResultSet rs=stmt.executeQuery("select * from student");
+			while(rs.next())
 			{
-				return true;
+				if(rollno==rs.getInt(1))
+				{
+					return true;
+				}
 			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
 		}
 		return false;
 	}
 	
 	public void updateStudent(Student s){
-		int index=0;
-		for(Student st:list)
-		{
-			if(st.getRollno()==s.getRollno())
-			{
-				break;
-			}
-			index++;
+		try {
+			PreparedStatement pstmt=conn.prepareStatement("update student set name=?, percentage=? where rollno=?");
+			pstmt.setString(1, s.getName());
+			pstmt.setDouble(2, s.getPercentage());
+			pstmt.setInt(3, s.getRollno());
+			pstmt.executeUpdate();
 		}
-		list.set(index, s);
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public boolean removeStudent(int rollno)
 	{
-		int index=0;
-		int flag=0;
-		for(Student st:list)
-		{
-			if(st.getRollno()==rollno)
-			{
-				flag=1;
-				break;
-			}
-			index++;
+		boolean status=false;
+		try {
+			PreparedStatement pstmt=conn.prepareStatement("delete from student where rollno=?");
+			pstmt.setInt(1, rollno);
+			status=pstmt.execute();
 		}
-		if(flag==0)
-			return false;
-		
-		list.remove(index);
-		return true;
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return !status;
 	}
+	
 }
